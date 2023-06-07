1b6abbffd7ae30021f42f33ef03a7bb9da15a165
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-06-07 13:16:21 +0100

    added script to remove subscription message on click/embedded user input into message

--- assets/js/emailValidation.js
+++ assets/js/emailValidation.js
@@ -14,19 +14,32 @@ See form validation messages if:
 
 
 function validateEmail(userInput) {
+    
+    
     // Tracks user input
     const emailValue = document.getElementById('email').value
-
+    
     // ASCII Characters:
     const validRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-
+    
     if (userInput.value.match(validRegex)) {
-    alert(`Thanks for subscribing to our newsletter ${emailValue}`) 
-    const displayMessage = document.getElementById('feedbackMessage')
+        // alert(`Thanks for subscribing to our newsletter ${emailValue}`) 
+    const triggerPopUp = document.getElementById('confirmationEmail')
+    triggerPopUp.removeAttribute('id')
+    triggerPopUp.classList.add('openPopUp')
+    
+    const subscriptionMessage = document.getElementById('subscriptionMessage')
+    const subscriptionText = document.createElement('p')
+    subscriptionText.textContent = `A confirmation email has been sent to ${emailValue}
+    Please open it and click the button inside to confirm your subscription.`
+    
+    // Takes user input and places it in subscription message
+    subscriptionMessage.append(subscriptionText)
 
     // Removes error message if email is formatted correctly
     displayMessage.textContent = ''
     } else {
+        document.getElementById('email').style.cssText = "border: 1px solid red; color: red; font-size: 20px; background-color: #ffb6c1;"
         // Dynamic error message for incorrect format
         const displayMessage = document.getElementById('feedbackMessage')
         const feedback = document.createElement('p')
@@ -35,3 +48,9 @@ function validateEmail(userInput) {
         displayMessage.append(feedback)
 }
 }
+
+// Function to close pop up message
+function closeConfirmation () {
+    const closePopUp = document.getElementById('popUpContainer')
+    closePopUp.classList.add('closePopUp')
+} 
